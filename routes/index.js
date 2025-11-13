5242715a22184e2f66277d73b3ed230e8d5a800e
Elizaveta006 <bakugoukattsuki111@gmail.com>
2025-11-13 11:51:41 +0300

    Пункт 4_2

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,25 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* Страница Довакина*/
+router.get('/dovakin', function(req, res, next) {
+    res.send("<h1>Страница Довакина</h1>")
+});
+
+/* Страница Алдуина*/
+router.get('/alduin', function(req, res, next) {
+    res.send("<h1>Страница Алдуина</h1>")
+});
+
+/* Страница Харкона*/
+router.get('/harkon', function(req, res, next) {
+    res.send("<h1>Страница Харкона</h1>")
+});
+
+/* Страница Мирака*/
+router.get('/mirak', function(req, res, next) {
+    res.send("<h1>Страница Мирака</h1>")
+});
+
 
 module.exports = router;
